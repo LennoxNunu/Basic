fe14a26fc31453f92f442846efc91ed114c20657
LennoxNunu <lennoxnunu@gmail.com>
2024-03-19 12:59:04 +0200

    starter

---
+++ Basic/RegistrationTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegistrationTableViewController.swift
+//  Basic
+//
+//  Created by Lennox Yandisa Nunu on 2024/03/19.
+//
+
+import Foundation
+import UIKit
+
+class RegistrationTableViewController : UITableViewController {
+    
+    @IBOutlet weak var emailTextField :UITextField!
+    @IBOutlet weak var passwordTextField :UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func save() {
+        
+        
+    }
+    
+}
